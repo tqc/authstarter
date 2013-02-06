8fef8f0271cafc9c3300b7eeb84b11cb464e8673
Tom Clarkson <tom@orangeguava.com>
2013-02-06 16:59:02 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+#Auth Starter
+
+This is the authentication code I find myself implementing on every project that needs a basic password protected demo or admin site. The flexibility of passport is nice, but for a simple app with few users all you need is something that works with minimal effort.
+
+* Based on passport-local
+* Username/password stored in mongodb
+* Limit unsuccessful login attempts (3 per minute by default)
+* Password hashing
+* Users cached in memory to avoid excessive db requests
+* Redirection to original url
+
+The following routes are added to the app:
+* GET /login
+* POST /login
+* GET /logout
+* GET /loginredirect
+
+## Installation
+
+    npm install authstarter
+
+To create necessary auth related view files, run
+
+    node
+    require("authstarter").setup();
+
+## Usage
+
+    var AuthStarter = require("./auth");
+
+    app.configure(function() {
+        app.use(express.cookieParser());
+        app.use(express.session({
+            secret: 'secret'
+        }));
+        app.use(express.bodyParser());
+    
+        AuthStarter.configure(app);
+        app.use(app.router);
+        app.use(express.static(__dirname + '/static'));
+        app.set('view engine', 'jshtml');
+    });
+
+
+    app.get('/', AuthStarter.ensureAuthenticated, function(req, res) {
+        req.send('Secured content');
+    });
+
+
+## User setup
+
+The user store is a mongodb collection containing documents like:
+
+    {
+      _id: ObjectId("537159a186915c696a000521"),
+      username: "username",
+      password: "password",
+      roles: {
+        admin: false
+      }
+    }
+
+Passwords may be either plain text or hashed in the format used by https://github.com/davidwood/node-password-hash
+
+Users may be created manually or using one of the provided functions that include password hashing.
+
+    AuthStarter.addUser("username", "password", {"user": true, "admin":false});
+
+    AuthStarter.setPassword(username, password);
+
+## Options
+
+	var settings = {
+        mongoUrl: process.env.MONGOHQ_URL,
+        baseUrl: process.env.SECURE_DOMAIN,
+        userCollection: 'AdminUsers',
+        hashOptions: {
+            algorithm: "sha512"
+        },
+        maxAttempts: 3
+    };
+
+    AuthStarter.configure(app, settings);
+
+* mongoUrl - a mongodb url as used by mongo-native
+* baseUrl - used to make redirects absolute. eg "https://example.com"
+* userCollection - name of the mongodb collection
+* hashOptions - as used by password-hash
+* maxAttempts - number of incorrect login attempts allowed within one minute
+
+
+
+
+
+
+
---
+++ authstarter.js
@@ -0,0 +1,204 @@
+(function() {
+var mongodb = require('mongodb');
+
+    var passport = require('passport');
+    var LocalStrategy = require('passport-local').Strategy;
+    
+    var passwordHash = require("password-hash");
+
+    var NodeCache = require( "nodea-cache" );    
+    var userCache = new NodeCache({ stdTTL: 100, checkperiod: 120});
+
+    var settings = {
+        mongoUrl: process.env.MONGOHQ_URL,
+        baseUrl: process.env.SECURE_DOMAIN,
+        userCollection: 'AdminUsers',
+        hashOptions: {
+            algorithm: "sha512"
+        },
+        maxAttempts: 3
+    };
+
+
+    var getUser = function(username, callback) {
+        userCache.get(username, function(err, val) {
+            if (val && val[username] !== undefined) return callback(val[username]);
+
+            mongodb.Db.connect(settings.mongoUrl, function(error, client) {
+            if(error) throw error;
+
+            new mongodb.Collection(client, settings.userCollection).findOne({
+                username: username
+            }, function(err, user) {               
+                client.close();
+                userCache.set(username, user);
+                return callback(user);
+            });
+        });
+
+        })
+
+
+    }
+
+
+    passport.use(new LocalStrategy(
+
+    function(username, password, done) {
+
+        getUser(username, function(user) {
+            if (!user) {
+                return done(null, false, {
+                            message: 'Invalid user/password'
+                        });
+            }
+
+            user.loginAttempts = user.loginAttempts || [];
+            var now = new Date().getTime();
+
+            user.loginAttempts.push(now);
+
+            if (user.loginAttempts.length > settings.maxAttempts) {
+                var then = user.loginAttempts.shift();
+                if (then > now - 60000) {
+                     return done(null, false, {
+                            message: 'Account locked'
+                        });
+                }
+            }
+
+
+
+            if (!passwordHash.isHashed(user.password)) {
+            if (password != user.password) {
+                return done(null, false, {
+                            message: 'Invalid user/password'
+                        });
+            }
+
+            }
+            else {
+
+            if (!passwordHash.verify(password, user.password)) {
+                return done(null, false, {
+                            message: 'Invalid user/password'
+                        });
+            }
+}
+
+            user.loginAttempts = [];
+            return done(null, user);
+        })    
+    }));
+
+
+    var ensureAuthenticated = function(req, res, next) {
+            if(process.env.AUTHTYPE == "skip") {
+                req.user = {
+                    username: "skipped login"
+                };
+                return next();
+            }
+            if(req.isAuthenticated()) {
+                return next();
+            }
+
+            req.session.callbackUrl = req.path;
+            res.redirect(settings.baseUrl + '/login');
+        }
+
+    passport.serializeUser(function(user, done) {
+        console.log(user);
+        done(null, user.username);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        getUser(id, function(user) {
+             return done(null, user);
+        });
+        console.log(id);
+    });
+
+
+    exports.configure = function(app, options) {
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+
+        var extend = require("extend");
+
+        extend(settings, options);
+
+        app.get('/logout', function(req, res) {
+            req.logout();
+            res.redirect(settings.baseUrl + '/');
+        });
+
+        app.get('/login', function(req, res) {
+            res.render('login', {
+                layout: "blanklayout",
+                title: '',
+                flash: req.flash()
+            });
+        });
+   
+        app.post('/login', passport.authenticate('local', {
+            successRedirect: settings.baseUrl + '/loginredirect',
+            failureRedirect: settings.baseUrl + '/login',
+            failureFlash: true
+        }));
+
+        app.get("/loginredirect", function(req, res) {
+            res.redirect(settings.baseUrl +( req.session.callbackUrl || "/"));
+        })
+
+
+
+    }
+
+
+    exports.ensureAuthenticated = ensureAuthenticated;
+
+    exports.addUser = function(username, password, roles) {
+          mongodb.Db.connect(settings.mongoUrl, function(error, client) {
+            if(error) throw error;
+            var collection = new mongodb.Collection(client, settings.userCollection);
+            collection.save({
+                username: username,
+                password: passwordHash.generate(password, settings.hashOptions),
+                roles: roles || { admin: false }
+            }, function() {               
+                client.close();
+            });
+        });
+    }
+
+    exports.setPassword = function(username, password) {
+
+       mongodb.Db.connect(settings.mongoUrl, function(error, client) {
+            if(error) throw error;
+            var collection = new mongodb.Collection(client, settings.userCollection);
+            collection.findOne({
+                username: username
+            }, function(err, user) {
+                if (user) {
+                    user.password = passwordHash.generate(password, settings.hashOptions);
+                     collection.save(user, function() {
+                        client.close();
+                        userCache.set(username, user);
+                     });
+                }else {
+                       client.close();
+                }
+            });
+        });
+    }
+
+    exports.setup = function() {
+        // todo: copy setup code from chondric
+        console.log("setup not implemented");
+        return "";
+    }
+
+})();
